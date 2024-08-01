7cf0ac3a3e6aa9a39e2f9b9d61fe0bcfec4c0e68
Alfonzo Villamor <57025746+mikolmax@users.noreply.github.com>
2024-08-02 00:59:44 +0800

    Add files via upload

---
+++ ToDoList/Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Hello!");
+
+Console.WriteLine("Input the first number:");
+var firstInput = Console.ReadLine();
+var firstNum = int.Parse(firstInput);
+
+Console.WriteLine("Input the second number:");
+var secondInput = Console.ReadLine();
+var secondNum = int.Parse(secondInput);
+
+Console.WriteLine("What do you want to do with these numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+string userChoice = Console.ReadLine();
+
+
+if (EqualsCaseInsensitive(userChoice, "A"))
+{
+    var sum = firstNum + secondNum;
+    PrintFinalEquation(firstNum, secondNum, sum, "+");
+}
+else if (EqualsCaseInsensitive(userChoice, "S"))
+{
+    var difference = firstNum - secondNum;
+    PrintFinalEquation(firstNum, secondNum, difference, "-");
+}
+else if (EqualsCaseInsensitive(userChoice, "M"))
+{
+    var multiplied = firstNum * secondNum;
+    PrintFinalEquation(firstNum, secondNum, multiplied, "*");
+}
+else
+{
+    Console.WriteLine("Invalid Option!");
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+void PrintFinalEquation (int num1, int num2, int result, string @operation)
+{
+    Console.WriteLine($"{num1} {@operation} {num2} = {result}");
+}
+
+bool EqualsCaseInsensitive (string left, string right)
+{
+    return left.ToUpper() == right.ToUpper();
+}
---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,136 @@
+﻿
+
+bool shallExit = false;
+
+var todos = new List<string>();
+
+Console.WriteLine("Hello");
+
+while (!shallExit)
+{
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    var userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+        case "s":
+        case "S":
+            SeeAllTodos();
+            break;
+        case "a":
+        case "A":
+            AddTodo();
+            break;
+        case "r":
+        case "R":
+            RemoveTodo();
+            break;
+        case "e":
+        case "E":
+            shallExit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid Choice");
+            break;
+    }
+}
+
+void SeeAllTodos()
+{
+    if (todos.Count == 0)
+    {
+        ShowNoTodosMessage();
+        return;
+    }
+
+    for (int i = 0; i < todos.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {todos[i]}");
+    }
+}
+
+void AddTodo()
+{
+    string description;
+
+    do
+    {
+        Console.WriteLine("Enter the TODO description:");
+        description = Console.ReadLine();
+    } while (!IsDescriptionValid(description));
+
+    todos.Add(description);
+}
+
+bool IsDescriptionValid(string description)
+{
+    if (description == "")
+    {
+        Console.WriteLine("The description cannot be empty");
+        return false;
+    }
+    else if (todos.Contains(description))
+    {
+        Console.WriteLine("The description must be unique");
+        return false;
+    }
+
+    return true;
+}
+
+void RemoveTodo()
+{
+    if (todos.Count == 0)
+    {
+        ShowNoTodosMessage();
+        return;
+    }
+
+    int index;
+
+    do
+    {
+        Console.WriteLine("Select the index of the TODO you want to remove:");
+        SeeAllTodos();
+    } while (!TryReadIndex(out index));
+
+    RemoveTodoAtIndex(index - 1);
+}
+
+bool TryReadIndex (out int index)
+{
+    var userInput = Console.ReadLine();
+
+    if (userInput == "")
+    {
+        index = 0;
+        Console.WriteLine("Selected index cannot be empty");
+        return false;
+    }
+
+    if (int.TryParse(userInput, out index) && index >= 1 && index <= todos.Count)
+    {
+        return true;
+    }
+
+    Console.WriteLine("The given index is not valid.");
+    return false;
+}
+
+void RemoveTodoAtIndex(int index)
+{
+    var todoToBeRemoved = todos[index];
+    todos.RemoveAt(index);
+
+    Console.WriteLine($"TODO removed: {todoToBeRemoved}");
+}
+
+static void ShowNoTodosMessage()
+{
+    Console.WriteLine("No TODOs have been added yet");
+}
